59b0ec460d5fd2119c6dc105b646254dacc13e8d
Jocelyn Lum <joce.lum@gmail.com>
2017-09-05 11:41:04 -0400

    deleted node modules

--- dev/app.js
+++ dev/app.js
@@ -1,1 +1,5 @@
-console.log('it works!');
+// app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Fact: The best ninja turtle is Donatello</h1>, document.getElementById('app'));
