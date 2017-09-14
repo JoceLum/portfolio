becf48163423e5ac04fc71f7941af85e7e86aa0e
Jocelyn Lum <joce.lum@gmail.com>
2017-09-14 12:33:58 -0400

    created general style sheet structure and modified gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 const gulp = require('gulp');
 const babel = require('babelify');
 const browserify = require('browserify');
+const sass = require('gulp-sass');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
+const concat = require('gulp-concat');
 
 gulp.task('styles', () => {
     return gulp.src('./dev/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('styles.css'))
-        .pipe(gulp.dest('/public/styles'))
+        .pipe(gulp.dest('./public/styles'))
 });
 
 
@@ -29,7 +31,7 @@ gulp.task('js', () => {
         }))
         .pipe(source('scripts.js'))
         .pipe(buffer())
-        .pipe(gulp.dest('public/scripts'))
+        .pipe(gulp.dest('./public/scripts'))
         .pipe(reload({ stream: true }));
 });
 
@@ -41,7 +43,8 @@ gulp.task('bs', () => {
     });
 });
 
-gulp.task('default', ['js', 'bs'], () => {
+gulp.task('default', ['js', 'bs', 'styles'], () => {
     gulp.watch('dev/**/*.js', ['js']);
-    gulp.watch('./public/styles.css', reload);
+    gulp.watch('dev/**/*.scss',['styles']);
+    gulp.watch('./public/styles/styles.css', reload);
 });
