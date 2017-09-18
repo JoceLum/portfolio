fd1b7bc4600657dcdffd942293d493c324942d45
Jocelyn Lum <joce.lum@gmail.com>
2017-09-18 19:55:11 -0400

    added more styling to all sections and added a favicon

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -1,16 +1,18 @@
 $(function() {
 
-//effect that changes appearance of nav bar as user scrolls down page	
+    //effect that changes appearance of nav bar as user scrolls down page   
     $(window).on("scroll", function() {
-        if ($(window).scrollTop() > $('#about').position().top) {
+        if ($(window).scrollTop() > $('.hero-section').position().top) {
             $('.nav').css('background-color', 'rgba(222, 222, 222, 0.9)');
+            $('.nav li').css('padding', '10px 20px')
 
         } else {
             $('.nav').css('background-color', 'transparent');
+            $('.nav li').css('padding', '20px')
         }
     });
 
-//smooth scroll
+    //smooth scroll
     $('a').click(function() {
         var link = $(this.hash);
         if (link.length > 0) {
@@ -21,15 +23,16 @@ $(function() {
         }
     });
 
-//Hamburger menu for mobile mode
-    $('.fa-bars').on('click', function(){
-    	if ($('.nav-mobile').css('display') == "flex"){
-    		$('.nav-mobile').css('display', 'none');
-    	} else {
-    		$('.nav-mobile').css('display', 'flex');
-    	}
+    //Hamburger menu for mobile mode
+    $('#nav-icon').on('click', function() {
+        $(this).toggleClass('open');
+        if ($('.nav-mobile').css('display') == "flex") {
+            $('.nav-mobile').css('display', 'none');
+        } else {
+            $('.nav-mobile').css('display', 'flex');
+        }
     })
 
-//to initialize wow.js for transition effects in portfolio section    
+    //to initialize wow.js for transition effects in portfolio section    
     new WOW().init();
 });
