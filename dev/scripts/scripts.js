0bc505a8f6ed79833025e702a28275106f2434c8
Jocelyn Lum <joce.lum@gmail.com>
2017-09-17 22:44:10 -0400

    added many styling changes at different media queries & created hamburger menu for mobile mode

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -1,12 +1,35 @@
+$(function() {
 
-new WOW().init(); 
+//effect that changes appearance of nav bar as user scrolls down page	
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > $('#about').position().top) {
+            $('.nav').css('background-color', 'rgba(222, 222, 222, 0.9)');
 
-$('a').click(function() {
-    var link = $(this.hash);
-    if (link.length > 0) {
-        $('html,body').animate({
-            scrollTop: link.offset().top
-        }, 1000);
-        return false;
-    }
+        } else {
+            $('.nav').css('background-color', 'transparent');
+        }
+    });
+
+//smooth scroll
+    $('a').click(function() {
+        var link = $(this.hash);
+        if (link.length > 0) {
+            $('html,body').animate({
+                scrollTop: link.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+
+//Hamburger menu for mobile mode
+    $('.fa-bars').on('click', function(){
+    	if ($('.nav-mobile').css('display') == "flex"){
+    		$('.nav-mobile').css('display', 'none');
+    	} else {
+    		$('.nav-mobile').css('display', 'flex');
+    	}
+    })
+
+//to initialize wow.js for transition effects in portfolio section    
+    new WOW().init();
 });
